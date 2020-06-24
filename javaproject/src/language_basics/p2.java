4e9bae34da94d4a1a97bf6b94a6be9c3240acee0
sri <sri@srilaxmi>
2020-06-24 22:25:23 +0530

    language basics

---
+++ javaproject/src/language_basics/p2.java
@@ -0,0 +1,12 @@
+package language_basics;
+import java.util.Scanner;
+public class p2 {
+
+	public static void main(String[] args) {
+		String s;
+		Scanner sc=new Scanner(System.in);
+		 s=sc.nextLine();
+		 System.out.println("Welcome "+s);
+	}
+
+}
---
+++ javaproject/src/language_basics/p3.java
@@ -0,0 +1,14 @@
+package language_basics;
+import java.util.Scanner;
+public class p3 {
+
+	public static void main(String[] args) {
+		int a,b,c;
+		Scanner sc=new Scanner(System.in);
+		a=sc.nextInt();
+		b=sc.nextInt();
+		c=a+b;
+		System.out.println(+c);
+	}
+
+}
